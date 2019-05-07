d3b7350d7ebb58fa69027e822653162ef95bde22
unknown <ben_ngo89@yahoo.com>
2019-05-07 09:17:15 -0400

    finished

---
+++ app/data/friends.js
@@ -0,0 +1,4 @@
+module.exports = [{
+    "name":"Grizzly",
+    "photo":"http://vignette3.wikia.nocookie.net/webarebears/images/2/2b/Grizzdancewalk.gif/revision/latest?cb=20150914213949","answers":["2","5","5","4","5","1","1","2","2","2"]
+}];
---
+++ server.js
@@ -0,0 +1,26 @@
+// Import dependencies.
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+
+var PORT = process.env.PORT || 8889;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname + '/app/public'));
+
+
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
